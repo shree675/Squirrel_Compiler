2588786aff7ac9a21c768937bb9b433b9f6afea9
Kranthi Sedamaki <skranthi4444@gmail.com>
2022-02-21 18:01:16 +0530

    start AST implementation
    Co-authored-by: Shreetesh M <shree675@users.noreply.github.com>

---
+++ SyntaxAnalysis/AstNode.py
@@ -0,0 +1,136 @@
+from enum import Enum
+
+class Operator(Enum):
+    A_AND = "&&"
+    A_OR = "||"
+    A_EQUAL = "=="
+    A_NOT_EQUAL = "!="
+    A_LESS_THAN = "<"
+    A_GREATER_THAN = ">"
+    A_LESS_THAN_EQUAL = "<="
+    A_GREATER_THAN_EQUAL = ">="
+    A_PLUS = "+"
+    A_MINUS = "-"
+    A_MULTIPLY = "*"
+    A_DIVIDE = "/"
+    A_MODULO = "%"
+    A_NOT = "!"
+    A_NEGATE = "-"
+    A_ASSIGN = "="
+    A_IF = "if"
+    A_ELSE = "else"
+    A_WHILE = "while"
+    A_FOR = "for"
+    A_RETURN = "return"
+    A_BREAK = "break"
+    A_VAR = "var"
+    A_CONST = "const"
+    A_FUNC = "func"
+    A_NODE = "node"
+    A_ROOT = "root"
+    A_DECL = "decl"
+    # A_CONTINUE = "continue"
+
+    
+class AstNode:
+    def __init__(self, operator=None, left=None, mid=None, right=None, value=None, next_label=None) :
+        self.operator = operator
+        self.left = left
+        self.mid = mid
+        self.right = right
+        self.value = value
+
+        self.true = None
+        self.false = None
+
+        self.code = "None"
+        self.next = next_label
+
+    @staticmethod
+    def generateCode(head, get_new_label):
+        if head is None: return
+        
+        if head.operator == Operator.A_ROOT:
+            AstNode.generateCode(head.left, get_new_label)
+            print(head.left.code)
+
+        # --------------------------------------------------
+
+        elif head.operator == Operator.A_NODE:
+            left, right = head.left, head.right
+            
+            if left and right:
+                right.next = head.next
+                AstNode.generateCode(right, get_new_label)
+                head.code += left.next + ":\n" + right.code     
+            elif left:
+                left.next = get_new_label()
+                AstNode.generateCode(left, get_new_label)
+                head.code = left.code + "\n"
+            
+            elif right:
+                right.next = head.next
+                AstNode.generateCode(right, get_new_label)
+                head.code = right.code
+
+        # ------------------------------------------------------------
+
+        elif head.operator == Operator.A_FUNC:
+            left, right = head.left, head.right
+            print('first:',right.code)
+
+            AstNode.generateCode(right,get_new_label)
+            print('second:',right.code)
+            
+            head.code = right.code + "\n" + head.next + ":\n" + "return\n"
+
+        # ------------------------------------------------------------
+
+        elif head.operator == Operator.A_AND:
+
+            left, right = head.left, head.right
+            true, false = head.true, head.false
+
+            left.true = get_new_label()
+            left.false = false
+            right.true = true
+            right.false = false
+
+            AstNode.generateCode(left, get_new_label)
+            AstNode.generateCode(right, get_new_label)
+
+            head.code = left.code + "\n" + left.true + ":" + "\n" + right.code + "\n"
+
+        # ------------------------------------------------------------
+
+        elif head.operator == Operator.A_OR:
+            left.true = true
+            left.false = get_new_label()
+            right.true = true
+            right.false = false
+
+            AstNode.generateCode(left, get_new_label)
+            AstNode.generateCode(right, get_new_label)
+
+            head.code = left.code + "\n" + left.false + ":" + "\n" + right.code + "\n"
+
+        # ------------------------------------------------------------
+            
+        elif head.operator == Operator.A_IF:
+            expr, statements = head.left, head.right
+            expr.true = get_new_label()
+            expr.false = head.next
+            statements.next = head.next
+            AstNode.generateCode(expr, get_new_label)
+            print('afjadfjlaas',statements.code)
+            head.code = expr.code + "\n" + expr.true + ":\n" + statements.code + "\n"
+
+        # ------------------------------------------------------------
+
+        elif head.operator == Operator.A_CONST:
+            if head.value == "true":
+                head.code = "goto " + head.true + "\n"
+            elif head.value == "false":
+                head.code = "goto " + head.false + "\n" 
+
+        # --------------------------------------------------------------------
--- SyntaxAnalysis/parser.py
+++ SyntaxAnalysis/parser.py
@@ -3,6 +3,7 @@ import sys
 import os
 sys.path.append(os.path.abspath('../LexicalAnalysis'))
 import lexer
+from AstNode import Operator, AstNode
 
 '''
 symbol_table : {
@@ -11,7 +12,7 @@ symbol_table : {
         'size' : 4,
         'line_no' : 1,
         'scope' : 'global',
-    }
+    
 }
 
 { id -> 1
@@ -92,24 +93,17 @@ class Parser(SlyParser):
         self.symbol_table = []
         self.id = 2
         self.scope_id_stack = [0, 1]
-        self.temp = -1
-        self.label = -1
-        self.next_label_stack = []
-        self.cond_label_stack = []
 
+        self.num_labels = 0
+
+    def get_new_label(self):
+        self.num_labels += 1
+        return "L" + str(self.num_labels - 1)
     
-    def gen_temp(self):
-        self.temp += 1
-        return "t"+str(self.temp)
-
-    def gen_label(self):
-        self.label += 1
-        return "L"+str(self.label)
-        
-
-    
-    debugfile = 'parser.out'
+    start = "program"
     tokens = lexer.Lexer.tokens
+
+    debugfile = 'parser.out'
 
     precedence = (
         ('left', 'COMMA'),
@@ -125,166 +119,85 @@ class Parser(SlyParser):
         ('right', 'PAREN')                # fictitious token
     )
 
+    # program -> methods
+    @_('methods')
+    def program(self, p):
+        val = AstNode(Operator.A_ROOT,left=p.methods)
+        val.left = p.methods
+        AstNode.generateCode(val, self.get_new_label)
 
-    
-    # statements -> statement; statements | e 
-    @_("statement statements")
+    # methods -> methods method
+    @_('methods method')
+    def methods(self, p):
+        return AstNode(Operator.A_NODE,left=p.methods, right=p.method)
+
+    @_('method')
+    def methods(self, p):
+        return AstNode(Operator.A_NODE,left=p.method)
+
+    # method -> DATATYPE FUNCNAME ( params ) { statements }
+    @_('DATATYPE FUNCNAME LPAREN params RPAREN LBRACE statements RBRACE')
+    def method(self,p):
+        return AstNode(Operator.A_FUNC,left=p.params,right=p.statements, next_label=self.get_new_label())
+
+    # params -> DATATYPE VARNAME, params | e
+    @_('DATATYPE VARNAME COMMA params')
+    def params(self,p):
+        return None
+
+    @_('empty')
+    def params(self,p):
+        return None
+
+    # statements -> statements statement 
+    @_("statements statement")
     def statements(self, p):
-        print("statements")
-        return {
-            "code" : p.statement["code"] + p.statements["code"],
-        }
+        return AstNode(Operator.A_NODE, left=p.statements, right=p.statement)
 
-    @_("")
+    @_('empty')
     def statements(self, p):
-        return {
-            "code" : "",
-        }
+        val = AstNode()
+        val.code = ""
+        return val
 
-    # temporary
-    # statement -> expr
-    #@_('expr')
-    #def statement(self, p):
-        #return p.expr
-    
     # statement -> declaration_statement | assignment_statement | io_statement | selection_statement | iteration_statement | jump_statement
     @_('declaration_statement SEMICOL')
     def statement(self, p):
-        return p[0]
+        return p.declaration_statement
 
     @_('assignment_statement SEMICOL')
     def statement(self, p):
-        return {
-            "code" : p.assignment_statement["code"]
-        }
+        pass
 
     @_('io_statement SEMICOL')
     def statement(self, p):
         return p[0]
 
-
-    @_('start_selection selection_statement')
+    @_('selection_statement')
     def statement(self, p):
-        self.next_label_stack.pop()
-        print("statement")
-        return {
-            "code" : p.selection_statement["code"] + p.selection_statement["next"] + "\n",
-        }
-    
-    @_("")
-    def start_selection(self, p):
-        self.next_label_stack.append(self.gen_label())
+        return p.selection_statement
     
     @_("if_statement")
     def selection_statement(self, p):
-        print("selection_statement")
-        return {
-            "code" : p.if_statement["code"],
-            "next" : self.next_label_stack[-1],
-        }
-
-    # @_('iteration_statement')
-    # def statement(self, p):
-    #     return p.[0]
-
-    # @_('jump_statement')
-    # def statement(self, p):
-    #     return p.[0]
-
+        return p.if_statement
+    
     # declaration_statement -> simple_init | array_init
     @_("simple_init")
     def declaration_statement(self, p):
-        return 0
+        return p.simple_init
 
     # simple_init -> DATATYPE VARNAME | DATATYPE VARNAME = expr
     @_("DATATYPE VARNAME")
     def simple_init(self, p):
-        cur_scope_vars = filter(lambda e : e["scope"] == self.scope_id_stack[-1] ,self.symbol_table)
-        for var in cur_scope_vars:
-            if var["identifier_name"] == p[1]:
-                print("Error: Variable already declared in current scope")
-                raise Exception(f"Error : variable \"{p[1]}\" already declared in current scope")
-
-        self.symbol_table.append({
-            "identifier_name" : p[1],
-            "type" : p[0],
-            "scope" : self.scope_id_stack[-1],
-            "parent_scope": self.scope_id_stack[-2]
-        })
-
-    # @_("IF LPAREN if_paren_open_and expr AND expr RPAREN LBRACE if_open statements if_close RBRACE")
-    # def if_statement(self, p):
-        # return {
-            # # "code" : p.expr["code"] + p.statements["code"],
-            # "code" : ""
-        # }
-
-    # @_("IF LPAREN if_paren_open_or expr OR expr RPAREN LBRACE if_open statements if_close RBRACE")
-    # def if_statement(self, p):
-        # return {
-            # # "code" : p.expr["code"] + p.statements["code"],
-            # "code" : ""
-        # }
-    
-    # @_("")
-    # def if_paren_open_and(self, p):
-        # self.cond_label_stack.append({"true" : self.gen_label(), "false" : self.next_label_stack[-1]})
-        # print("b1_open")
-        # self.cond_label_stack.append({"true" : self.gen_label(), "false" : self.cond_label_stack[-1]["false"]})
-        # return {
-            # "true" : self.cond_label_stack[-1]["true"],
-            # "false" : self.cond_label_stack[-1]["false"]
-        # }
-
-    # @_("")
-    # def if_paren_open_or(self, p):
-        # self.cond_label_stack.append({"true" : self.gen_label(), "false" : self.next_label_stack[-1]})
+        val = AstNode(Operator.A_DECL, left=p.VARNAME)
+        val.code = p.DATATYPE + " " + p.VARNAME
+        return val
 
 
     # if_statement -> IF ( expr ) { statements if_close }
-    @_("IF if_paren_open LPAREN expr RPAREN LBRACE if_open statements if_close RBRACE")
+    @_("IF LPAREN expr RPAREN LBRACE statements RBRACE")
     def if_statement(self, p):
-        print("if_statement")
-        return {
-            "code" : p.expr["code"] + self.cond_label_stack[-1]["true"] + "\n" + p.statements["code"],
-            # "code" : ""
-        }
-    
-    @_("bool_expr PLUS expr")
-    def bool_expr(self, p):
-
-        addr = self.gen_temp()
-
-        return {
-            "addr" : addr,
-            "code" : p.bool_expr["code"] + p.expr["code"] + f"{addr} = {p.bool_expr['addr']} + {p.expr['addr']}" + "\n"
-        }
-    
-    @_("constant")
-    def bool_expr(self, p):
-        return p.constant
-
-    @_("")
-    def if_open(self, p):
-        self.scope_id_stack.append(self.id)
-        self.id += 1
-
-    @_("")
-    def if_close(self, p):
-        self.scope_id_stack.pop()
-        self.cond_label_stack.pop()
-    
-    @_("")
-    def if_paren_open(self, p):
-        self.cond_label_stack.append({"true" : self.gen_label(), "false" : self.next_label_stack[-1]})
-
-        # print("b1_open")
-        # self.cond_label_stack.append({"true" : self.gen_label(), "false" : self.cond_label_stack[-1]["false"]})
-        # return {
-            # "true" : self.cond_label_stack[-1]["true"],
-            # "false" : self.cond_label_stack[-1]["false"]
-        # }
-        
+        return AstNode(Operator.A_IF, left=p.expr, right=p.statements)
 
     # io_statement -> input_statement | output_statement
     @_('input_statement')
@@ -334,64 +247,29 @@ class Parser(SlyParser):
             expr1.code
             t = expr0.addr + expr1.addr
         '''
-        addr = self.gen_temp()
-
-        return {
-            "addr" : addr,
-            "code" : p.expr0["code"] + p.expr1["code"] + f"{addr} = {p.expr0['addr']} + {p.expr1['addr']}" + "\n"
-        }
+        pass
 
     @_('expr MINUS expr')
     def expr(self, p):
-
-        addr = self.gen_temp()
-
-        return {
-            "addr" : addr,
-            "code" : p.expr0["code"] + p.expr1["code"] + f"{addr} = {p.expr0['addr']} - {p.expr1['addr']}" + "\n"
-        }
+        pass
 
 
     @_('expr MULT expr')
     def expr(self, p):
-
-        addr = self.gen_temp()
-
-        return {
-            "addr" : addr,
-            "code" : p.expr0["code"] + p.expr1["code"] + f"{addr} = {p.expr0['addr']} * {p.expr1['addr']}" + "\n"
-        }
+        pass
 
     @_('expr DIVIDE expr')
     def expr(self, p):
-
-        addr = self.gen_temp()
-
-        return {
-            "addr" : addr,
-            "code" : p.expr0["code"] + p.expr1["code"] + f"{addr} = {p.expr0['addr']} / {p.expr1['addr']}" + "\n"
-        }
+        pass
 
     @_('expr MOD expr')
     def expr(self, p):
-
-        addr = self.gen_temp()
-
-        return {
-            "addr" : addr,
-            "code" : p.expr0["code"] + p.expr1["code"] + f"{addr} = {p.expr0['addr']} % {p.expr1['addr']}" + "\n"
-        }
+        pass
 
 
     @_('MINUS expr %prec UMINUS')
     def expr(self, p):
-
-        addr = self.gen_temp()
-        
-        return {
-            "addr" : addr,
-            "code": p.expr["code"] + f"{addr} = -{p.expr['addr']}" + "\n"
-        }
+        pass
 
     @_('LPAREN expr RPAREN %prec PAREN')
     def expr(self, p):
@@ -406,36 +284,22 @@ class Parser(SlyParser):
     @_('expr RELOP2 expr')
     def expr(self, p):
         return str('('+p.expr0+p[1]+p.expr1+')')
-
-  
-
-    @_('b1_open expr b2_open AND expr')
-    def expr(self, p):
-        print("b1_open expr b2_open OR expr")
-        return {
-            "code" : p.expr0["code"] + p.b1_open["true"] + "\n" + p.expr1["code"]
-            # "code" : p.expr1["code"]
-        } 
     
     @_("COMMA")
     def b1_open(self, p):
-        print("b1_open")
-        self.cond_label_stack.append({"true" : self.gen_label(), "false" : self.cond_label_stack[-1]["false"]})
-        return {
-            "true" : self.cond_label_stack[-1]["true"],
-            "false" : self.cond_label_stack[-1]["false"]
-        }
+        pass
 
     @_('')
     def b2_open(self, p):
         print("b2_open")
-        self.cond_label_stack.pop()
-        self.cond_label_stack.append({"true" : self.cond_label_stack[-1]["true"], "false" : self.cond_label_stack[-1]["false"]})
+        
+    @_('expr AND expr')
+    def expr(self, p):
+        return AstNode(Operator.A_AND, left=p.expr0, right=p.expr1)        
 
-    # @_('expr OR expr')
-    # def expr(self, p):
-        # print('expr OR expr')
-        # return str('('+p.expr0+p[1]+p.expr1+')')
+    @_('expr OR expr')
+    def expr(self, p):
+        return AstNode(Operator.A_OR, left=p.expr0, right=p.expr1)
 
     @_('NOT expr %prec NOT')
     def expr(self, p):
@@ -453,11 +317,7 @@ class Parser(SlyParser):
     # expr -> constant
     @_('constant')
     def expr(self, p):
-        print("constant " + p.constant["val"])
-        return {
-            "addr" : p.constant["val"],
-            "code" : p.constant["code"]
-        }
+        return AstNode(Operator.A_CONST,value=p.constant)
 
     # expr -> (DATATYPE) expr
     @_('LPAREN DATATYPE RPAREN expr %prec TYPECASTING')
@@ -535,10 +395,7 @@ class Parser(SlyParser):
 
     @_('BOOLVAL')
     def constant(self, p):
-        return {
-            "val" : p.BOOLVAL,
-            "code" : "goto " + self.cond_label_stack[-1][p.BOOLVAL] + "\n"
-        }
+        return p.BOOLVAL
 
     # expr -> function_call
     @_('function_call')
@@ -567,10 +424,14 @@ class Parser(SlyParser):
     def argument(self, p):
         return str(p[0])
 
+    @_('')
+    def empty(self, p):
+        pass
+
     #def error(self, p):
         #if p:
             #print("Syntax error at line", p.lineno, "| TOKEN:", p.value)
-            #self.errok()
+            #
         #else:
             #print("Syntax error at EOF")
         #raise Exception('Syntax error')
@@ -582,16 +443,7 @@ if __name__ == '__main__':
     lex = lexer.Lexer()
     parser = Parser()
 
+    with open("../TestSuites/TACtest.sq", 'r') as f:
+        text = f.read()
 
-    while True:
-        # try:
-            # text = input('calc > ')
-            file = open("../TestSuites/TACtest.sq", 'r')
-            text = file.read()
-            result = parser.parse(lex.tokenize(text))
-            print(result["code"])
-            # print(parser.symbol_table)
-            break
-        # except:
-            # print('Error')
-            # break
+    parser.parse(lex.tokenize(text))
